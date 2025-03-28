f3891ea4446f48eb6bc4c686a947dd86d944a65b
Shubham91999 <skulka86@asu.edu>
2025-03-28 00:07:13 -0400

    Updated model and main

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,36 @@
-from flask import Request, jsonify
+from flask import request, jsonify
 from config import app, db
 from model import Contact
 
+# Route to show all the saved contacts in our application
 @app.route("/contacts", methods=["GET"])
 def get_contacts():
-    contacts = Contact.query.all()
+    contacts = Contact.query.all() # 
     json_contacts = list(map(lambda x: x.to_json(), contacts))
     return jsonify({"contacts": json_contacts})
 
+# Route to add a new contact in our application
+@app.route("/add_contact", methods=["POST"])
+def create_contact():
+    # Breaking down the request object into required attributes
+    first_name = request.json.get("firstName")
+    last_name = request.json.get("lastName")
+    email = request.json.get("email")
 
+    # Checking for nullness
+    if not first_name or not last_name or not email:
+        return (jsonify({
+            "message": "You must enter first name, last name and email"
+        }), 400)
+
+    new_contact = Contact(first_name=first_name, last_name=last_name, email=email) # Create new Contact object if all entered
+    try:
+        db.session.add(new_contact)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+    
+    return jsonify({"message": "Contact created successfully"}), 201   # Status code 201 (Created) is more specific
 
 
 if __name__ == "__main__":
--- backend/model.py
+++ backend/model.py
@@ -5,13 +5,13 @@ from config import db
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80), nullable=False, unique=False)
-    second_name = db.Column(db.String(80), nullable=False, unique=False)
+    last_name = db.Column(db.String(80), nullable=False, unique=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
 
     def to_json(self):
         return {
             "id": self.id,
             "firstName": self.first_name,
-            "secondName": self.second_name,
+            "lastName": self.last_name,
             "email": self.email
         }
