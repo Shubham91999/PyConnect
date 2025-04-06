59685b7693bed78e8196bc50f4fbae7d84d64334
Shubham91999 <skulka86@asu.edu>
2025-04-05 20:14:36 -0700

    Integrating Update contact

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import ContactForm from './ContactForm'
 
 function App() {
   const [contacts, setContacts] = useState([]);
-  const [isModelOpen, setIsModelOpen] = useState([])
+  const [isModelOpen, setIsModelOpen] = useState([false])
 
   useEffect(() => {
     fetchContacts()
@@ -16,14 +16,32 @@ function App() {
     {/* Getting response object by fetch method calling our contacts API */}
     const response = await fetch("http://127.0.0.1:5000/contacts")   
     const data = await response.json()
-    setContacts(data.contacts)
-    console.log(data.contacts)
+    setContacts(data.contacts) 
+  }
+
+  const closeModel = () => {
+    setIsModelOpen(false)
+  }
+
+  const openCreateModel = () => {
+    if (!isModelOpen) setIsModelOpen(true)
   }
 
 
-  return <><ContactList contacts={contacts}/>
-          <ContactForm/>
+  return (<>
+    <ContactList contacts={contacts}/>
+          <button onClick={openCreateModel}>Create New Contact</button>
+          {
+            isModelOpen && <div className="modal">
+              <div className="modal-content">
+                <span className="close" onClick={closeModel}>&times;</span>
+                  <ContactForm />
+              </div>
+            </div>
+          }
+         
         </>
+  );
 } 
 
 export default App
